6387e59d8ea72042d9438458a7a140193e19a8ff
kyotakata <57854222+kyotakata@users.noreply.github.com>
2023-05-30 07:20:09 +0900

    プロジェクト ファイルを追加します。

---
+++ テスト容易性Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("テスト容易性Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("テスト容易性Tests")]
+[assembly: AssemblyCopyright("Copyright © HP 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ec33654a-12fe-4250-9d8f-30866216b292")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ テスト容易性Tests/UnitTest2.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using テスト容易性;
+
+namespace テスト容易性Tests
+{
+    [TestClass]
+    internal class UnitTest2:TestBase
+    {
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var vm = new AViewModel();
+            Assert.AreEqual(50, vm.GetValue());
+
+            SettingA.PowerMode = true;
+            Assert.AreEqual(100, vm.GetValue());
+
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var vm = new BViewModel();
+
+            Assert.AreEqual(50, vm.GetValue());
+
+            SettingA.PowerMode = true;
+            Assert.AreEqual(100, vm.GetValue());
+
+        }
+
+    }
+}
